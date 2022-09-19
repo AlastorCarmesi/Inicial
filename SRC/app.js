a5afd0fa2e1b71c5cc2fb271884b53e640820334
AlastorCarmesi <carmesiblog7@gmail.com>
2022-09-19 12:59:11 -0500

    Servidor

--- SRC/app.js
+++ SRC/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const log = require('morgan');
 const app = express();
+const bodyParser = require('body-parser');
+const indexRoutes = require('./Routes/index.js');
 
 //escuchar servidor
 app.set('port', process.env.PORT || 3000);
+
+//middleware
+app.use(log('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+//Routes
+app.use("/",indexRoutes);
+
 app.listen(app.get('port'),()=> {
     console.log('servidor operando',app.get('port'))
 });
