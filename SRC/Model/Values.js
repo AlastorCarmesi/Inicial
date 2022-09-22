52048d2f6e36d623bfcc843942d1f4eaf9183b9a
AlastorCarmesi <carmesiblog7@gmail.com>
2022-09-22 12:49:23 -0500

    Formulario y Agregando campos

--- SRC/Model/Values.js
+++ SRC/Model/Values.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Val = new Schema({
-    Temp: String,
-    Hum: String,
-    Dia: Date
+    Email: String,
+    Cont: String,
+    NomUs: String,
+    Nom: String
+
 
 });
 module.exports = mongoose.model('valores',Val);
