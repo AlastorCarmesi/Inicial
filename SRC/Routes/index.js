b0e43fb91aacb9111d3c4ea861618dd316ddf6e1
AlastorCarmesi <carmesiblog7@gmail.com>
2022-10-24 11:21:17 -0500

    Actualizacion

--- SRC/Routes/index.js
+++ SRC/Routes/index.js
@@ -12,19 +12,27 @@ router.post('/add', async (req, res) => {
     const Val = new Valor(req.body);
     await Val.save();
     res.redirect('/');
+
 });
 
-router.get('/del/:id', async(res, req) => {
-    const id = req.params;
+router.get('/Del/:id',async(res, req) => {
+    const id = req.params.id;
     await Val.findByIdAndRemove(id);
     res.redirect('/');
+    
 });
 
-router.get('/ver', async (req, res)=>{
+router.get('/Ver', async (req, res)=>{
     const Val = await Valor.find();
     console.log(Val);
     res.status(200).json({reg: Val})
 });
+
+router.get('/Editar', async (req, res)=>{
+    const Email = await Email.find();
+    console.log(Email);
+    res.status(200).json({reg: Email});
+})
 
 module.exports = router;
 
