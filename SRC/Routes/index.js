afd8408c9fe65757834a7a27b33a3cd2e69d266d
AlastorCarmesi <carmesiblog7@gmail.com>
2022-10-24 11:32:46 -0500

    Actualizacion

--- SRC/Routes/index.js
+++ SRC/Routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const model = require('../model/Values')();
-const Valor = require('../model/Values');
+const model = require('../model/valores')();
+const Valor = require('../model/valores');
  
 
 router.get('/', (req, res) =>{
@@ -35,4 +35,3 @@ router.get('/Editar', async (req, res)=>{
 })
 
 module.exports = router;
-
